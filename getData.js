89b529c7237bf98677b274800620950453d566d2
saurav022 <65665827+saurav022@users.noreply.github.com>
2021-06-07 11:55:55 +0530

    Uploded the file

---
+++ getData.js
@@ -0,0 +1,36 @@
+function fetchData()
+{
+   fetch("https://reqres.in/api/users")
+   .then(response => {
+      if(!response.ok){
+         throw Error("error");
+      }
+      return response.json();
+      
+})
+
+.then(metadata=>{
+   const getData=metadata.data
+   .map(user=>{
+      return `
+      
+      <div class="user">
+         <p>FirstName: ${user.first_name}</p>
+         <p>LastName: ${user.last_name}</p>
+         <p>FullName: ${user.first_name}  ${user.last_name}</p>
+         <p>Email:${user.email}</p>
+      </div>`;
+      
+   })
+   .join("");
+   document.getElementById("app").innerHTML = getData;
+   
+   //document.querySelector('#app').insertAdjacentHTML("afterbegin", getData);
+})
+.catch(error=>{
+   console.log(error);
+});
+}
+//fetchData();
+
+
